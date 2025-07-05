b02d27649261acac9cdd3214f9532745f3e244a4
mrfatworm <mrfatworm@gmail.com>
2025-07-05 10:48:00 +0800

    refactor: Improve HomeScreenSingle Performance

--- composeApp/src/commonMain/kotlin/feature/home/presentation/HomeScreenSingle.kt
+++ composeApp/src/commonMain/kotlin/feature/home/presentation/HomeScreenSingle.kt
@@ -6,11 +6,9 @@
 package feature.home.presentation
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import feature.banner.components.AnnouncementBanner
@@ -27,35 +25,44 @@ import ui.utils.verticalSafePadding
 
 @Composable
 fun HomeScreenSingle(
-    uiState: HomeState,
-    onAction: (HomeAction) -> Unit,
-    onOpenBannerDialog: () -> Unit
+    uiState: HomeState, onAction: (HomeAction) -> Unit, onOpenBannerDialog: () -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())
-            .padding(horizontalSafePadding()).padding(verticalSafePadding()),
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().padding(horizontalSafePadding()),
+        contentPadding = verticalSafePadding(),
         verticalArrangement = Arrangement.spacedBy(contentGap())
     ) {
-        AnnouncementBanner(
-            uiState.banner,
-            onActionClicked = onOpenBannerDialog,
-            onClosed = {
-                onAction(HomeAction.DismissBanner(it))
-            }
-        )
-        CoverImageCard(uiState.coverImage)
-        NewsPagerCard(uiState.newsList)
-        HoYoLabCard(uiState.gameRecord, uiState.signResult, onSignClick = {
-            onAction(HomeAction.Sign)
-        }, onAddAccountClick = {
-            onAction(HomeAction.NavigateTo(Screen.HoYoLabSync.route))
-        }, onMyAgentClick = {
-            onAction(HomeAction.NavigateTo(Screen.MyAgentsList.route))
-        })
-        PixivCard(uiState.pixivTopics) {
-            onAction(HomeAction.ChangePixivTag(it))
+        item {
+            AnnouncementBanner(
+                uiState.banner, onActionClicked = onOpenBannerDialog, onClosed = {
+                    onAction(HomeAction.DismissBanner(it))
+                })
         }
-        TwitterCard(uiState.allForum.twitter)
-        AllForumCard(uiState.allForum)
+        item {
+            CoverImageCard(uiState.coverImage)
+        }
+        item {
+            NewsPagerCard(uiState.newsList)
+        }
+        item {
+            HoYoLabCard(uiState.gameRecord, uiState.signResult, onSignClick = {
+                onAction(HomeAction.Sign)
+            }, onAddAccountClick = {
+                onAction(HomeAction.NavigateTo(Screen.HoYoLabSync.route))
+            }, onMyAgentClick = {
+                onAction(HomeAction.NavigateTo(Screen.MyAgentsList.route))
+            })
+        }
+        item {
+            PixivCard(uiState.pixivTopics) {
+                onAction(HomeAction.ChangePixivTag(it))
+            }
+        }
+        item {
+            TwitterCard(uiState.allForum.twitter)
+        }
+        item {
+            AllForumCard(uiState.allForum)
+        }
     }
 }
