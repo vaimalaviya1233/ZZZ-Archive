97a1359f0fc3a0a4dd3b7a4d8e9696a3451d10b8
mrfatworm <mrfatworm@gmail.com>
2025-09-06 17:06:13 +0800

    refactor: Forum Response null-safety

--- composeApp/src/commonMain/kotlin/feature/forum/data/mapper/AllForumMapper.kt
+++ composeApp/src/commonMain/kotlin/feature/forum/data/mapper/AllForumMapper.kt
@@ -1,73 +1,78 @@
+/*
+ * Copyright 2025 The ZZZ Archive Open Source Project by mrfatworm
+ * License: MIT
+ */
+
 package feature.forum.data.mapper
 
 import feature.forum.model.AllForumResponse
 import feature.forum.model.AllForumState
-import feature.forum.model.BahamutForumListItem
+import feature.forum.model.BahamutForumListItemResponse
 import feature.forum.model.BahamutForumListState
-import feature.forum.model.NgaForumListItem
+import feature.forum.model.NgaForumListItemResponse
 import feature.forum.model.NgaForumListState
-import feature.forum.model.PttForumListItem
+import feature.forum.model.PttForumListItemResponse
 import feature.forum.model.PttForumListState
 import feature.forum.model.RedditForumListItem
 import feature.forum.model.RedditForumListState
-import feature.forum.model.TwitterForumListItem
+import feature.forum.model.TwitterForumListItemResponse
 import feature.forum.model.TwitterForumListState
 
 fun AllForumResponse.toAllForumState(): AllForumState {
     return AllForumState(
-        reddit = reddit.map { it.toRedditForumListState() },
-        bahamut = bahamut.map { it.toBahamutForumListState() },
-        ptt = ptt.map { it.toPttForumListState() },
-        twitter = twitter.map { it.toTwitterForumListState() },
-        nga = nga.map { it.toNgaForumListState() },
+        reddit = reddit?.map { it.toRedditForumListState() } ?: emptyList(),
+        bahamut = bahamut?.map { it.toBahamutForumListState() } ?: emptyList(),
+        ptt = ptt?.map { it.toPttForumListState() } ?: emptyList(),
+        twitter = twitter?.map { it.toTwitterForumListState() } ?: emptyList(),
+        nga = nga?.map { it.toNgaForumListState() } ?: emptyList(),
     )
 }
 
 fun RedditForumListItem.toRedditForumListState(): RedditForumListState {
     return RedditForumListState(
-        title = title,
-        link = link,
-        imgUrl = image,
+        title = title.orEmpty(),
+        link = link.orEmpty(),
+        imgUrl = image.orEmpty(),
         upVotes = upVotes.toString(),
         comments = comments.toString(),
         score = score.toString()
     )
 }
 
-fun BahamutForumListItem.toBahamutForumListState(): BahamutForumListState {
+fun BahamutForumListItemResponse.toBahamutForumListState(): BahamutForumListState {
     return BahamutForumListState(
-        title = splitBahamutText(title).second,
-        category = splitBahamutText(title).first,
-        link = link,
-        imgUrl = imgSrc,
+        title = splitBahamutText(title.orEmpty()).second,
+        category = splitBahamutText(title.orEmpty()).first,
+        link = link.orEmpty(),
+        imgUrl = imgSrc.orEmpty(),
         gp = gp.toString()
     )
 }
 
-fun PttForumListItem.toPttForumListState(): PttForumListState {
+fun PttForumListItemResponse.toPttForumListState(): PttForumListState {
     return PttForumListState(
-        link = link,
-        title = title,
-        popularity = if (popularity >= 100) "爆" else popularity.toString(),
-        source = source
+        link = link.orEmpty(),
+        title = title.orEmpty(),
+        popularity = if ((popularity ?: 0) >= 100) "爆" else popularity.toString(),
+        source = source.orEmpty()
     )
 }
 
-fun TwitterForumListItem.toTwitterForumListState(): TwitterForumListState {
+fun TwitterForumListItemResponse.toTwitterForumListState(): TwitterForumListState {
     return TwitterForumListState(
-        title = title,
-        link = link,
-        imgUrl = image,
-        author = author,
-        authorUrl = authorProfile,
-        hashtag = hashtag
+        title = title.orEmpty(),
+        link = link.orEmpty(),
+        imgUrl = image.orEmpty(),
+        author = author.orEmpty(),
+        authorUrl = authorProfile.orEmpty(),
+        hashtag = hashtag.orEmpty()
     )
 }
 
-fun NgaForumListItem.toNgaForumListState(): NgaForumListState {
+fun NgaForumListItemResponse.toNgaForumListState(): NgaForumListState {
     return NgaForumListState(
-        title = title,
-        link = link,
+        title = title.orEmpty(),
+        link = link.orEmpty(),
         replies = replies.toString()
     )
 }
--- composeApp/src/commonMain/kotlin/feature/forum/model/AllForumResponse.kt
+++ composeApp/src/commonMain/kotlin/feature/forum/model/AllForumResponse.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025 The ZZZ Archive Open Source Project by mrfatworm
+ * License: MIT
+ */
+
 package feature.forum.model
 
 import kotlinx.serialization.SerialName
@@ -5,92 +10,101 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AllForumResponse(
-    @SerialName("baha") val bahamut: List<BahamutForumListItem>,
-    @SerialName("nga") val nga: List<NgaForumListItem>,
-    @SerialName("ptt") val ptt: List<PttForumListItem>,
-    @SerialName("x") val twitter: List<TwitterForumListItem>,
-    @SerialName("reddit") val reddit: List<RedditForumListItem>
+    @SerialName("baha") val bahamut: List<BahamutForumListItemResponse>? = null,
+    @SerialName("nga") val nga: List<NgaForumListItemResponse>? = null,
+    @SerialName("ptt") val ptt: List<PttForumListItemResponse>? = null,
+    @SerialName("x") val twitter: List<TwitterForumListItemResponse>? = null,
+    @SerialName("reddit") val reddit: List<RedditForumListItem>? = null
 )
 
 @Serializable
-data class BahamutForumListItem(
-    val link: String, val imgSrc: String, val title: String, val gp: Int
+data class BahamutForumListItemResponse(
+    val link: String? = null,
+    val imgSrc: String? = null,
+    val title: String? = null,
+    val gp: Int? = null
 )
 
 @Serializable
-data class NgaForumListItem(
-    val link: String, val title: String, val replies: Int
+data class NgaForumListItemResponse(
+    val link: String? = null,
+    val title: String? = null,
+    val replies: Int? = null
 )
 
 @Serializable
-data class PttForumListItem(
-    val link: String, val title: String, val popularity: Int, val source: String
+data class PttForumListItemResponse(
+    val link: String? = null,
+    val title: String? = null,
+    val popularity: Int? = null,
+    val source: String? = null
 )
 
 @Serializable
-data class TwitterForumListItem(
-    val title: String,
-    val link: String,
-    val image: String,
-    val author: String,
-    val authorProfile: String,
-    val hashtag: String
+data class TwitterForumListItemResponse(
+    val title: String? = null,
+    val link: String? = null,
+    val image: String? = null,
+    val author: String? = null,
+    val authorProfile: String? = null,
+    val hashtag: String? = null
 )
 
 @Serializable
 data class RedditForumListItem(
-    val title: String,
-    val link: String,
-    val image: String,
-    @SerialName("upvotes") val upVotes: Int,
-    val comments: Int,
-    val score: Int
+    val title: String? = null,
+    val link: String? = null,
+    val image: String? = null,
+    @SerialName("upvotes")
+    val upVotes: Int? = null,
+    val comments: Int? = null,
+    val score: Int? = null
 )
 
 val stubAllForumResponse = AllForumResponse(
     bahamut = listOf(
-        BahamutForumListItem(
+        BahamutForumListItemResponse(
             link = "https://forum.gamer.com.tw/C.php?bsn=74860&snA=4527&tnum=1",
             imgSrc = "https://truth.bahamut.com.tw/s01/202412/forum/7486/ce837662563ab6a49ab8bbd321005428.JPG?w=300&h=300&fit=o",
             title = "【情報】邦布們的說明書｜格列佛探員",
             gp = 35
-        ), BahamutForumListItem(
+        ), BahamutForumListItemResponse(
             link = "https://forum.gamer.com.tw/C.php?bsn=74860&snA=4539&tnum=1",
             imgSrc = "https://truth.bahamut.com.tw/s01/202412/forum/74860/b0a64872f780d94dfcd97f69a9aa61c8.JPG?w=300&h=300&fit=o",
             title = "【繪圖】雅在進行對抗寒冷的修行",
             gp = 67
         )
     ), nga = listOf(
-        NgaForumListItem(
+        NgaForumListItemResponse(
             link = "https://bbs.nga.cn/read.php?tid=42659556",
             title = "[新闻] 《绝区零》1.4版本&#39;星流霆击&#39;前瞻信息汇总   ",
             replies = 34
-        ), NgaForumListItem(
+        ), NgaForumListItemResponse(
             link = " https ://bbs.nga.cn/read.php?tid=42756365",
             title = "[闲聊杂谈] b580这么强吗？不知道稳定性怎样",
             replies = 22
         )
     ), ptt = listOf(
-        PttForumListItem(
+        PttForumListItemResponse(
             link = "https://www.ptt.cc/bbs/miHoYo/M.1734358450.A.8EA.html",
             title = "Re: [絕區] 星見雅的故事告訴了我們什麼？",
             popularity = 4,
             source = "miHoYo"
-        ), PttForumListItem(
+        ), PttForumListItemResponse(
             link = "https://www.ptt.cc/bbs/miHoYo/M.1734354816.A.20F.html",
             title = "Fw: [絕區] 星見雅的故事告訴了我們什麼？",
             popularity = 1,
             source = "miHoYo"
         )
     ), twitter = listOf(
-        TwitterForumListItem(
+        TwitterForumListItemResponse(
             title = "Miyabi 🐱",
             link = "https://x.com/zeltdraws/status/1868666139464876477",
             image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm0t_HPWKblrfxE-osoUIh9HHdKF3FE9BIXIniVDy5oDhouokc4QRKEJ4&s=0",
             author = "Zeltdraws/Comms open 2/5 slots",
             authorProfile = "https://x.com/zeltdraws",
             hashtag = "#ゼンゼロ #젠존제 #zzzero"
-        ), TwitterForumListItem(
+        ), TwitterForumListItemResponse(
             title = "Astra Yao Menú — Zenless Zone Zero",
             link = "https://x.com/KdeKovaK/status/1868569844918174154",
             image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbsp8u8GcMLwXiyEfrSAQ-fuTyg9mCoUHc4Sv5OhAjn_bWXnQVMwD-wbs&s",
